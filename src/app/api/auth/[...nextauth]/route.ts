130eda83c418bc950ea78e0710bda0ec784ee43d
martin2844 <martin2844@gmail.com>
2024-09-07 16:45:49 +0200

    fix: Remove dependencies from Mui

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,5 @@
+import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-
-import { findOrCreateUser } from "@/services/user";
-
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    // ...add more providers here
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXT_AUTH_SECRET,
-  callbacks: {
-    async signIn({ user }: any) {
-      try {
-        await findOrCreateUser(user);
-      } catch (error) {
-        console.log(error);
-      }
-      return true;
-    },
-  },
-};
 
 const handler = NextAuth({
   ...authOptions,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/authOptions";
 
 import Link from "next/link";
 const HomePage = async () => {
