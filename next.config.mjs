bdcdde8626c39f127042b4c83341313ec5dfde07
Martin <martin2844@gmail.com>
2024-11-29 14:04:30 +0100

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    instrumentationHook: true,
-    serverComponentsExternalPackages: ["knex"],
-  },
+  serverExternalPackages: ["knex"],
   output: "standalone",
 };
 
